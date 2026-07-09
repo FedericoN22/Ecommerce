c7df39b387901fefae832f0c2e73ded556686cea
FedericoN22 <fedenuul@gmail.com>
2026-07-09 15:24:40 -0300

    gitignore

--- E-commerceApi/obj/Debug/net8.0/E-commerceApi.AssemblyInfo.cs
+++ E-commerceApi/obj/Debug/net8.0/E-commerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2f010a16e63cfdd64ceaec8eef47fb8f1775ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f0ab515a170cdce24c7786f4d2ad13f08ee7db")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
